f909c6da9d51d7ce26518d20604446dac422b3e8
Jörn Diedrichsen <joern.diedrichsen@googlemail.com>
2021-10-28 22:09:02 -0400

    dictlearning

--- dict_learn.py
+++ dict_learn.py
@@ -2,6 +2,7 @@ from sklearn.decomposition import dict_learning, sparse_encode
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 def random_V(K=5,N=20):
     V = np.random.normal(0,1,(K,N))
@@ -13,34 +14,42 @@ def random_U(P=100,K=5,type='onegamma',alpha=2,beta=0.3):
     if type=='onegamma':
         u = np.random.choice(K,(P,))
         g = np.random.gamma(alpha,beta,(P,))
-        U = np.zeros((K,P))
+        U = np.zeros((P,K))
         for i in np.arange(K):
-            U[i, u==i] = g[u==i]
+            U[u==i,i] = g[u==i]
     elif type=='iidgamma':
         U = np.random.gamma(alpha,beta,(P,K))
     elif type=='one':
         u = np.random.choice(K,(P,))
-        U = np.zeros((K,P))
+        U = np.zeros((P,K))
         for i in np.arange(K):
             U[u==i, i] = 1
     return U
 
-def random_Y(U,V,eps):
-    N = V.shape[0]
-    P = U.shape[1]
-    Y = U @ V + np.random.normal(0,eps/np.sqrt(N),(P,N)) 
+def random_Y(U,V,eps=1):
+    N = V.shape[1]
+    P = U.shape[0]
+    Y = U @ V + np.random.normal(0,eps/np.sqrt(N),(P,N))
+    return Y
 
 def check_consistency(P=100,K=5,N=40):
     num=10
     U = random_U(P,K,'onegamma')
     V = random_V(K,N)
+    Y = random_Y(U,V,eps=1)
     Uhat = np.empty((num,P,K))
     Vhat = np.empty((num,K,N))
+    iter = np.empty((num,))
+    loss = np.empty((num,))
+
     for i in range(num):
-        # Determine random starting value 
+        # Determine random starting value
         V_init = random_V(K,N)
-        U_init = sparse_encode(Y,V_init,alpha=1,algorithm='lasso_cd')
-        Uhat[i,:,:],Vhat[i,:,:],errors = dict_learning (Y,alpha = 1, n_components=5, method='cd',random_state=i,positive_code=True,code_init=U_init, dict_init=V_init)
+        U_init = np.random.uniform(0,1,(P,N))
+        Uhat[i,:,:],Vhat[i,:,:],errors = dict_learning (Y,alpha = 0.1, n_components=5,
+            method='cd',positive_code=True,code_init=U_init, dict_init=V_init)
+        loss[i] = errors[-1]
+        iter[i] = len(errors)
     pass
 
 if __name__ == '__main__':
