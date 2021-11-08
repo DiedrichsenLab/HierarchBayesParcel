2b648f680480b73462e6fa28bd8beab63dae8369
Jörn Diedrichsen <joern.diedrichsen@googlemail.com>
2021-11-08 16:11:57 -0500

    correspondence_cleanup

--- dict_learn.py
+++ dict_learn.py
@@ -34,9 +34,9 @@ def random_Y(U,V,eps=1):
 
 def check_consistency(P=100,K=5,N=40):
     num=10
-    U = random_U(P,K,'onegamma')
+    U = random_U(P,K,'iidgamma')
     V = random_V(K,N)
-    Y = random_Y(U,V,eps=1)
+    Y = random_Y(U,V,eps=3)
     Uhat = np.empty((num,P,K))
     Vhat = np.empty((num,K,N))
     iter = np.empty((num,))
@@ -46,11 +46,107 @@ def check_consistency(P=100,K=5,N=40):
         # Determine random starting value
         V_init = random_V(K,N)
         U_init = np.random.uniform(0,1,(P,N))
-        Uhat[i,:,:],Vhat[i,:,:],errors = dict_learning (Y,alpha = 0.1, n_components=5,
-            method='cd',positive_code=True,code_init=U_init, dict_init=V_init)
+        Uhat[i,:,:],Vhat[i,:,:],errors,iter[i] = dict_learning (Y,alpha = 0.1, n_components=K,
+            method='cd',positive_code=True,
+            code_init=U_init, dict_init=V_init,
+            return_n_iter=True,max_iter=200)
         loss[i] = errors[-1]
-        iter[i] = len(errors)
+    i = np.argsort(loss)
+    loss = loss[i]
+    iter = iter[i]
+    Vhat = Vhat[i,:,:]
+    _,M = vmatch(Vhat,Vhat)
+    plt.imshow(M)
     pass
 
+def dict_learn_rep(Y,K=5,num=1):
+    num_subj,P,N = Y.shape
+    Vsubj = np.empty((num_subj,K,N))
+    Vhat = np.empty((num,K,N))
+    iter = np.empty((num,))
+    loss = np.empty((num,))
+    for s in range(num_subj):
+        print(f"subj:{s}")
+        Vhat = np.empty((num,K,N))
+        for i in range(num):
+            # Determine random starting value
+            V_init = random_V(K,N)
+            U_init = np.random.uniform(0,1,(P,N))
+            Uhat,Vhat[i,:,:],errors,iter[i] = dict_learning(Y[s,:,:],alpha = 0.1, n_components=K,
+            method='cd',positive_code=True,
+            code_init=U_init, dict_init=V_init,
+            return_n_iter=True,max_iter=200)
+            loss[i] = errors[-1]
+        # Sort the solutions by the loss
+        i=loss.argmin()
+        Vsubj[s,:,:] = Vhat[i,:,:]
+    return Vsubj
+
+def vmatch(V1,V2):
+    """Gets the mean minimal distances of every vector in V1
+        to any vector in V2.
+    """
+    num_sub,K1,N = V1.shape
+    num_sub,K2,N = V2.shape
+    M = np.zeros((num_sub,num_sub)) # Mean correspondence
+    for i in range(num_sub):
+        for j in range(num_sub):
+            if i==j:
+                M[i,j]=np.nan
+            else:
+                M[i,j]=(V1[i,:,:] @ V2[j,:,:].T).max(axis=1).mean()
+    return np.nanmean(M,axis=1), M
+
+
+def vmatch_baseline(K=[5,5],N=30,num=20):
+    V = np.empty((2,),'object')
+    for set in range(2):
+        V[set]=np.empty((num,K[set],N))
+        for i in range(num):
+            V[set][i,:,:]=random_V(K[set],N)
+    vm,M = vmatch(V[0],V[1])
+    return np.mean(vm)
+
+def vmatch_baseline_fK():
+    kmax= 30
+    vm = np.empty((kmax,))
+    kk = np.arange(1,kmax+1)
+    for k in kk:
+        vm[k-1] = vmatch_baseline([k,3],60)
+    plt.plot(kk,vm)
+    pass
+
+def correspondence_sim(K=[3,8],N=42,P=100,sig=[2,2]):
+    # Checks on the power of correspondence test between
+    num=1
+    num_subj=3
+
+    # Generate data
+    V = np.empty((2,),'object')
+    U = np.empty((2,),'object')
+    Y = np.empty((2,),'object')
+    for set in range(2):
+        V[set] = random_V(K[set],N)
+        U[set] = np.empty((num_subj,P,K[set]))
+        Y[set] = np.empty((num_subj,P,N))
+        for s in range(num_subj):
+            U[set][s,:,:] = random_U(P,K[set])
+            Y[set][s,:,:] = random_Y(U[set][s,:,:],V[set],sig[set])
+
+    # Estimate V repeatedly
+    Vhat=np.empty((2,),'object')
+    for set in range(2):
+        Vhat[set]=dict_learn_rep(Y[set],K=K[set])
+
+    # Establish match within and across sets
+    match = [[0,0],[0,1],[1,1],[1,0]]
+    M = np.zeros((4,num_subj))
+    for l,m in enumerate(match):
+        M[l,:],mV = vmatch(Vhat[m[0]],Vhat[m[1]])
+    pass
+
+
 if __name__ == '__main__':
+    # vmatch_baseline_fK()
+    # correspondence_sim()
     check_consistency()
