5ef6e5f1caa5ff79407dee2ad5f5121a6f922136
Jörn Diedrichsen <joern.diedrichsen@googlemail.com>
2022-01-02 23:00:21 -0500

    RBM - refactorization

--- rbm.py
+++ rbm.py
@@ -4,9 +4,9 @@ import spatial as sp
 import matplotlib.pyplot as plt
 
 class RBM():
-    def __init__(self, width):
-        self.grid = sp.SpatialGrid(width=width,height=width)
-        nh = nv = self.grid.P
+    def __init__(self, nh, nv):
+        self.nh = nh
+        self.nv = nv
         self.W = pt.randn(nv,nh)
         self.bh = pt.randn(nh)
         self.bv = pt.randn(nv)
@@ -25,10 +25,97 @@ class RBM():
         sample_v = pt.bernoulli(p_v_given_h)
         return p_v_given_h, sample_v
 
-    def train(self, v0, vk, ph0, phk, alpha=0.5):
-        self.W += alpha * pt.mm(ph0.t(),v0) - pt.mm(phk.t(),vk)
-        self.bv += alpha * pt.sum((v0 - vk), 0)
-        self.bh += alpha * pt.sum((ph0 - phk), 0)
+    def sample(self,num_subj,iter=5):
+        """Draw new subjects from the model
+
+        Args:
+            num_subj (int): Number of subjects
+            iter (int): Number of iterations until burn in
+        """
+        v = pt.empty(num_subj,self.nv).uniform_(0,1)
+        v = v>0.5
+        v = v.type(pt.float)
+        for i in range (iter):
+            _,h = self.sample_h(v)
+            _,v = self.sample_v(h)
+        return v,h
+
+    def epos(self, v):
+        self.epos_v = v
+        wv = pt.mm(v, self.W.t())
+        activation = wv + self.bh
+        self.epos_Eh = pt.sigmoid(activation)
+        return self.epos_Eh
+
+    def eneg_CDk(self,v,iter = 1,ph=None):
+        for i in range(iter):
+            _,h = self.sample_h(v)
+            _,v = self.sample_v(h)
+        self.eneg_Eh ,_ = self.sample_h(v)
+        self.eneg_v = v
+        return self.eneg_v,self.eneg_Eh
+
+    def eneg_pCD(self,num_chains=None,iter=3):
+        if (self.eneg_v is None) or (self.eneg_v.shape[0]!=num_chains):
+            v = pt.empty(num_chains,self.nv).uniform_(0,1)
+            v = v>0.5
+        else:
+            v= self.eneg_v
+        for i in range(iter):
+            _,h = self.sample_h(v)
+            _,v = self.sample_v(h)
+        self.eneg_Eh ,_ = self.sample_h(v)
+        self.eneg_v = v
+        return self.eneg_v,self.eneg_Eh
+
+    def Mstep(self,alpha=0.8):
+        N = self.epos_Eh.shape[0]
+        M = self.eneg_Eh.shape[0]
+        self.W += alpha * (pt.mm(self.epos_Eh.t(),self.epos_v) - N / M * pt.mm(self.eneg_Eh.t(),self.eneg_v))
+        self.bv += alpha * pt.sum((self.epos_v - N / M * self.eneg_v), 0)
+        self.bh += alpha * pt.sum((self.epos_Eh - N / M * self.eneg_Eh), 0)
+
+    def evaluate_test(self,v,hidden,lossfcn='abserr'):
+        wh = pt.mm(hidden, self.W)
+        activation = wh + self.bv
+        p_v = pt.sigmoid(activation)
+        if lossfcn=='abserr':
+            loss = pt.sum(v * pt.log(p_v) + (1-v) * pt.log(1-p_v))
+        elif lossfcn=='loglik':
+            loss = pt.sum(pt.abs(v-p_v))
+        return loss
+
+    def evaluate_completion(self,v,part,lossfcn='abserr'):
+        num_part = part.max()+1
+        loss = pt.zeros(self.nv)
+        for k in range(num_part):
+            ind = part==k
+            v0 = pt.detach(v)
+            v0[:,ind] = 0.5 # Agnostic input
+            p_h = pt.sigmoid(pt.mm(v0, self.W.t()) + self.bh)
+            # sample_h = pt.bernoulli(p_h)
+            p_v = pt.sigmoid(pt.mm(p_h, self.W) + self.bv)
+            # sample_v = pt.bernoulli(p_v_given_h)
+            if lossfcn=='abserr':
+                loss[ind] = pt.sum(pt.abs(v[:,ind] - p_v[:,ind]),0)
+            elif lossfcn=='loglik':
+                loss[ind] = pt.sum(v[:,ind] * pt.log(p_v[:,ind]) + (1-v[:,ind]) * pt.log(1-p_v[:,ind]),0)
+        return pt.sum(loss)
+
+class RBM_grid(RBM):
+    def __init__(self, width = 5, type='neighbor'):
+        self.grid = sp.SpatialGrid(width=width,height=width)
+        P = self.grid.P
+        super().__init__(P,P)
+        if type=='neighbor':
+            self.grid.W = 1*self.grid.W + 2*np.eye(P)
+            self.W = pt.tensor(self.grid.W,dtype=pt.float32)
+            self.bh = -pt.ones(P) * self.W.sum(0).mean()/2
+            self.bv = -pt.ones(P) * self.W.sum(0).mean()/2
+        elif type == 'random':
+            pass
+        else:
+            raise(NameError(f'unkown type: {type}'))
 
 def plot_batch(v0,vk,ph0,phk,rbm):
     plt.figure()
@@ -43,66 +130,56 @@ def plot_batch(v0,vk,ph0,phk,rbm):
         plt.subplot(4,N,n+3*N+1)
         rbm.grid.plot_maps(phk[n],vmax=1,cmap='jet')
 
-
 def train_RBM():
     # Make true hidden data set
-    width = 4
-    N = 200
-    rbm_t = RBM(width)
-    P =rbm_t.grid.P
-    rbm_t.grid.W = 1*rbm_t.grid.W + 2*np.eye(P)
-    rbm_t.W = pt.tensor(rbm_t.grid.W,dtype=pt.float32)
-    rbm_t.bh = -pt.ones(P) * rbm_t.W.sum(0).mean()/2
-    rbm_t.bv = -pt.ones(P) * rbm_t.W.sum(0).mean()/2
-    ph = pt.empty(N,P).uniform_(0,1)
-    h_train=pt.bernoulli(ph)
-    # plt.figure()
-    for k in range(6):
-        # plt.subplot(2,6,k+1)
-        # rbm_t.grid.plot_maps(ph[0],cmap='jet',vmin=0,vmax=1)
-        pv,v_train=rbm_t.sample_v(h_train)
-        # plt.subplot(2,6,k+7)
-        # rbm_t.grid.plot_maps(pv[0],cmap='jet',vmin=0,vmax=1)
-        ph,h_train=rbm_t.sample_h(v_train)
-    pass
-    # Now generate new RBM and use for fitting
-    rbm = RBM(width)
-    # rbm.grid.W = 2*rbm.grid.W + 4*np.eye(P)
-    # rbm.W = pt.tensor(rbm.grid.W,dtype=pt.float32)
-    # rbm.bh = -pt.ones(P) * rbm.W.sum(0).mean()/2
-    # rbm.bv = -pt.ones(P) * rbm.W.sum(0).mean()/2
+    loss = 'abserr'
 
+    width = 4
+    N = 100
+    rbm_t = RBM_grid(width=width,type='random')
+    vtrain, hidden = rbm_t.sample(N,iter=100)
+    _,vtest = rbm_t.sample_v(hidden) # Get independent test data
+    num_part = 4
+    p=pt.ones(num_part)/num_part
+    part = pt.multinomial(p,rbm_t.nv,replacement=True)
+    rbm = RBM_grid(width=width,type='random')
     nb_epoch = 20
-    batch_size = 20
-    loss_train = np.zeros(nb_epoch)
+    batch_size = 50
+    ll_train = np.zeros(nb_epoch)
+    ll_test1 = np.zeros(nb_epoch)
+    ll_test2 = np.zeros(nb_epoch)
 
     for epoch in range(nb_epoch):
-        for n in range(0, N - batch_size, batch_size):
-            vk = v_train[n: n+batch_size]
-            v0 = v_train[n: n+batch_size]
-            ph0,_ = rbm.sample_h(v0)
-            for k in range(10):
-                _, hk = rbm.sample_h(vk)
-                _, vk = rbm.sample_v(hk)
-            phk, _ = rbm.sample_h(vk)
-            rbm.train(v0, vk, ph0, phk,alpha=0.01)
-            loss_train[epoch] += pt.mean(pt.abs(v0-vk))
-            # plot_batch(v0,vk,ph0,phk,rbm)
-            pass
-        print(f"Epoch {epoch}, Loss {loss_train[epoch]:.3f}")
-    # Now generate test_loss
-    plt.plot(loss_train)
-    test_loss = 0
-    s = 0.
-    for id_user in range(0, nb_users):
-        v_input = training_set[id_user: id_user+1]
-        v_target = test_set[id_user: id_user+1]
-        if len(v_target(v_target>=0)):
-            _, h = rbm.sample_h(v_input)
-            _, v_input = rbm.sample_v(h)
-        test_loss += torch.mean(torch.abs(v_target[v_target>0]-
-                                          v_input[v_target>0]))
-        s += 1
+
+        # Get test error
+        Eh = rbm.epos(vtrain)
+        ll_train[epoch] = rbm.evaluate_test(vtrain,Eh,loss=loss)
+        ll_test1[epoch]= rbm.evaluate_test(vtest,Eh,loss=loss)
+        ll_test2[epoch]= rbm.evaluate_completion(vtest,part,loss=loss)
+        print(f'epoch {epoch:2d} Train: {ll_train[epoch]:.1f}, Test1: {ll_test1[epoch]:.1f}, Test2: {ll_test2[epoch]:.1f}')
+
+        for b in range(0,N-batch_size,batch_size):
+            ind = range(b,b+batch_size)
+            rbm.epos(vtrain[ind,:])
+            v,Eh=rbm.eneg_CDk(vtrain[ind,:],iter=1)
+            rbm.Mstep(alpha=0.01)
+
+    # Get true log likelihood
+    Eh = rbm_t.epos(vtrain)
+    ll_true_train = rbm_t.evaluate_test(vtest,Eh,loss=loss)
+    ll_true1 = rbm_t.evaluate_test(vtest,Eh,loss=loss)
+    ll_true2 = rbm_t.evaluate_completion(vtest,part,loss=loss)
+    ll_base_train = rbm_t.evaluate_baseline(vtest,loss=loss)
+    ll_base_test = rbm_t.evaluate_baseline(vtest,loss=loss)
+
+    t=np.arange(0,nb_epoch)
+    plt.plot(t,ll_train,'k',t,ll_test1,'r',t,ll_test2,'r:')
+    plt.axhline(y=ll_true1,color='r',ls='-')
+    plt.axhline(y=ll_true2,color='r',ls=':')
+    plt.axhline(y=ll_true_train,color='k',ls='-')
+    plt.axhline(y=ll_base_train,color='b',ls='-')
+    plt.axhline(y=ll_base_test,color='b',ls='-')
+    pass
 
 if __name__ == '__main__':
     train_RBM()
