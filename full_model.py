abdbb777759bedb6548c201ef3e22b74158db6d7
dzhi1993 <r930710@gmail.com>
2022-02-13 15:48:07 -0500

    update full model

--- full_model.py
+++ full_model.py
@@ -169,14 +169,84 @@ class FullModel:
             raise NameError('Parameter name needs to be model.param')
 
 
-def _fit_full(Y):
-    pass
-
-
 def _plot_loglike(loglike, true_loglike, color='b'):
     plt.figure()
     plt.plot(loglike, color=color)
     plt.axhline(y=true_loglike, color='r', linestyle=':')
+
+
+def _plot_diff(theta_true, theta, K, name='V'):
+    """ Plot the model parameters differences.
+
+    Args:
+        theta_true: the params from the true model
+        theta: the estimated params
+        color: the line color for the differences
+    Returns: a matplotlib object to be plot
+    """
+    iter = theta.shape[0]
+    diff = np.empty((iter, K))
+    Y = np.split(theta_true, K)
+    for i in range(iter):
+        x = np.split(theta[i], K)
+        for j in range(len(x)):
+            dist = np.linalg.norm(x[j] - Y[j])
+            diff[i, j] = dist
+    plt.figure()
+    plt.plot(diff)
+    plt.title('the differences: true %ss, estimated %ss' % (name, name))
+
+
+def _plt_single_param_diff(theta_true, theta, name=None):
+    plt.figure()
+    if name is not None:
+        plt.title('The difference: true %s vs estimated %s' % (name, name))
+
+    iter = theta.shape[0]
+    theta_true = np.repeat(theta_true, iter)
+    plt.plot(theta_true, linestyle='--', color='r')
+    plt.plot(theta, color='b')
+
+
+def generate_data(emission, k=2, dim=3, p=1000,
+                  num_sub=10, beta=1, alpha=1, signal_type=0):
+    model_name = ["GMM", "GMM_exp", "GMM_gamma", "VMF"]
+    arrangeT = ArrangeIndependent(K=k, P=p, spatial_specific=False)
+    U = arrangeT.sample(num_subj=num_sub)
+    if signal_type == 0:
+        signal = np.random.exponential(beta, (num_sub, p))
+    elif signal_type == 1:
+        signal = np.random.gamma(alpha, beta, (num_sub, p))
+    else:
+        raise ValueError("The value of signal strength must satisfy a distribution, 0 - exponential; 1 - gamma.")
+
+    if emission == 0:  # GMM
+        emissionT = MixGaussian(K=k, N=dim, P=p)
+    elif emission == 1:  # GMM with exponential signal strength
+        emissionT = MixGaussianExp(K=k, N=dim, P=p)
+    elif emission == 2:  # GMM with gamma signal strength
+        emissionT = MixGaussianGamma(K=k, N=dim, P=p)
+    elif emission == 3:
+        emissionT = MixVMF(K=k, N=dim, P=p)
+    else:
+        raise ValueError("The value of emission must be 0(GMM), 1(GMM_exp), 2(GMM_gamma), or 3(VMF).")
+
+    if (emission == 1) or (emission == 2):
+        data = emissionT.sample(U, signal)
+    elif emission == 3:
+        data = emissionT.sample(U)
+        signal = np.repeat(signal[:, np.newaxis, :], dim, axis=1)
+        data = data * signal
+    else:
+        data = emissionT.sample(U)
+
+    return data, U
+
+
+def sample_spherical(npoints, ndim=3):
+    vec = np.random.randn(ndim, npoints)
+    vec /= np.linalg.norm(vec, axis=0)
+    return vec
 
 
 def _simulate_full_GMM(K=5, P=100, N=40, num_sub=10, max_iter=50):
@@ -196,7 +266,7 @@ def _simulate_full_GMM(K=5, P=100, N=40, num_sub=10, max_iter=50):
     # loglike_true = np.sum(Uhat * emll_true) + np.sum(ll_a)
     # print(theta_true)
     T = FullModel(arrangeT, emissionT)
-    T, ll, theta = T.fit_em(Y=Y, iter=1, tol=0.00001)
+    T, ll, theta, _ = T.fit_em(Y=Y, iter=1, tol=0.00001)
     loglike_true = ll
 
     # Step 3: Generate new models for fitting
@@ -205,7 +275,7 @@ def _simulate_full_GMM(K=5, P=100, N=40, num_sub=10, max_iter=50):
 
     # Step 4: Estimate the parameter thetas to fit the new model using EM
     M = FullModel(arrangeM, emissionM)
-    M, ll, theta = M.fit_em(Y=Y, iter=max_iter, tol=0.00001)
+    M, ll, theta, _ = M.fit_em(Y=Y, iter=max_iter, tol=0.00001)
     _plot_loglike(np.trim_zeros(ll, 'b'), loglike_true, color='b')
     print('Done.')
 
@@ -259,7 +329,7 @@ def _simulate_full_VMF(K=5, P=100, N=40, num_sub=10, max_iter=50):
     # loglike_true = np.sum(Uhat * emll_true) + np.sum(ll_a)
     # print(theta_true)
     T = FullModel(arrangeT, emissionT)
-    T, ll, theta = T.fit_em(Y=Y, iter=1, tol=0.00001)
+    T, ll, theta, _ = T.fit_em(Y=Y, iter=1, tol=0.00001)
     loglike_true = ll
 
     # Step 3: Generate new models for fitting
@@ -269,12 +339,13 @@ def _simulate_full_VMF(K=5, P=100, N=40, num_sub=10, max_iter=50):
 
     # Step 4: Estimate the parameter thetas to fit the new model using EM
     M = FullModel(arrangeM, emissionM)
-    M, ll, theta = M.fit_em(Y=Y, iter=max_iter, tol=0.00001)
+    M, ll, theta, _ = M.fit_em(Y=Y, iter=max_iter, tol=0.00001)
     _plot_loglike(np.trim_zeros(ll, 'b'), loglike_true, color='b')
     print('Done.')
 
 
 if __name__ == '__main__':
-    # _simulate_full_VMF(K=5, P=1000, N=40, num_sub=10, max_iter=50)
+    _simulate_full_VMF(K=5, P=1000, N=40, num_sub=10, max_iter=50)
     # _simulate_full_GMM(K=5, P=1000, N=20, num_sub=10, max_iter=100)
-    _simulate_full_GME(K=5, P=1000, N=20, num_sub=10, max_iter=100)
+    # _simulate_full_GME(K=5, P=1000, N=20, num_sub=10, max_iter=100)
+
