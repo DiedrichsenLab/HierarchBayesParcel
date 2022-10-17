8bb4c4899fd26de589a67607c03c8a532c5b680c
Joern Diedrichsen <joern.diedrichsen@googlemail.com>
2022-10-17 15:44:22 -0400

    alignment across emission models

--- notebooks/test_em.py
+++ notebooks/test_em.py
@@ -176,7 +176,7 @@ def simulate_full_VMF2(X, K=5, P=100, num_sub=10, max_iter=20,
     plt.tight_layout()
     plt.show()
 
-def simulate_full_VMF2(X, K=5, P=100, num_sub=10, max_iter=20,
+def simulate_multi(X, K=5, P=100, num_sub=[10], max_iter=20,
                        uniform_kappa=True, missingdata=None, 
                        part_vec = None, n_inits=10):
     """Simulation function used for testing full model with a VMF emission
@@ -194,16 +194,21 @@ def simulate_full_VMF2(X, K=5, P=100, num_sub=10, max_iter=20,
     Returns:
         Several evaluation plots.
     """
-    N = X.shape[0]
+    n_sets = len(X)
+    if part_vec is None: 
+        part_vec = [None]*n_sets
 
     # Step 1: Set the true model to some interesting value
     arrangeT = ArrangeIndependent(K=K, P=P, spatial_specific=False, remove_redundancy=False)
-    emissionT = MixVMF(K=K, N=X.shape[0], P=P, X=X, uniform_kappa=uniform_kappa,part_vec=part_vec)
+    emissionT = []
+    for i in range(n_sets): 
+        emissionT.append(MixVMF(K=K, P=P, N=X[i].shape[0], X=X[i],
+                                uniform_kappa=uniform_kappa,
+                                part_vec=part_vec[i]))
 
     # Step 2: Generate data by sampling from the above model
-    T = FullMultiModel(arrangeT, [emissionT])
-    U = arrangeT.sample(num_subj=num_sub)
-    Y = emissionT.sample(U)
+    T = FullMultiModel(arrangeT, emissionT)
+    U,Y = T.sample(num_subj=num_sub)
 
     if missingdata is not None:
         idx = pt.randint(0, P-1, (num_sub, int(missingdata*P)))
@@ -213,41 +218,39 @@ def simulate_full_VMF2(X, K=5, P=100, num_sub=10, max_iter=20,
 
 
     # Step 3: Compute the log likelihood from the true model
-    Uhat_true, loglike_true = T.Estep(Y=[Y])  # Run only Estep!
+    Uhat_true, loglike_true = T.Estep(Y)  # Run only Estep!
     theta_true = T.get_params()
 
     arrangeM = ArrangeIndependent(K=K, P=P, spatial_specific=False, remove_redundancy=False)
-    emissionM = MixVMF(K=K, N=N, P=P, X=X, uniform_kappa=uniform_kappa,part_vec=part_vec)
-    M = FullMultiModel(arrangeM, [emissionM])
+    emissionM = []
+    for i in range(n_sets): 
+        emissionM.append(MixVMF(K=K, P=P, N=X[i].shape[0], X=X[i],
+                        uniform_kappa=uniform_kappa,
+                        part_vec=part_vec[i]))
+    M = FullMultiModel(arrangeM, emissionM)
 
-    M, ll, theta, U_hat, first_ll = M.fit_em_ninits(Y=[Y], iter=max_iter, first_iter=6,
-                                  tol=0.001, fit_arrangement=True)
+    M, ll, theta, U_hat, first_ll = M.fit_em_ninits(Y=Y, 
+                iter=max_iter, first_iter=6, 
+                tol=0.001, fit_arrangement=True)
 
     # Plot fitting EM -results
-    fig, axs = plt.subplots(1, 3, figsize=(12, 4))
-    _plot_loglike(axs[0], first_ll[:,1:].T, loglike_true, color='b')
-    axs[0].plot(ll[1:], color='r')
+    fig, axs = plt.subplots(n_sets, 3, figsize=(12, 8))
+    _plot_loglike(axs[0,0], first_ll[:,1:].T, loglike_true, color='b')
+    axs[0,0].plot(ll[1:], color='r')
 
-    ind = M.get_param_indices('emissions.0.V')
-    true_V = theta_true[ind].reshape(emissionM.M, K)
-    predicted_V = theta[:, ind]
-    idx = matching_params(true_V, predicted_V, once=False)
-    _plot_diff(axs[1], true_V, predicted_V, index=idx, name='V')
+    for i in range(n_sets):
+        ind = M.get_param_indices(f'emissions.{i}.V')
+        true_V = theta_true[ind].reshape(emissionM[i].M, K)
+        predicted_V = theta[:, ind]
+        idx = matching_params(true_V, predicted_V, once=False)
+        _plot_diff(axs[i,1], true_V, predicted_V, index=idx, name='V')
 
-    ind = M.get_param_indices('emissions.0.kappa')
-    if uniform_kappa or emissionT.uniform_kappa:
+        ind = M.get_param_indices(f'emissions.{i}.kappa')
         # Plot if kappa is uniformed
-        _plt_single_param_diff(axs[2], 
-                    theta_true[T.get_param_indices('emissions.0.kappa')],
+        _plt_single_param_diff(axs[i,2], 
+                    theta_true[T.get_param_indices(f'emissions.{i}.kappa')],
                     theta[:, ind], name='kappa')
-    else:
-        # Plot if kappa is not uniformed
-        idx = matching_params(theta_true[ind].reshape(1, K), 
-                            theta[:, ind], once=False)
-        _plot_diff(axs[2], theta_true[ind].reshape(1, K), 
-                            theta[:, ind], index=idx, name='kappa')
-
-    fig.suptitle('VMF fitting results, run = %d' % int(emissionT.X.shape[0]/emissionT.X.shape[1]))
+   
     plt.tight_layout()
     plt.show()
 
@@ -255,4 +258,5 @@ if __name__ == '__main__':
     # Test full model fitting: 
     X = pt.eye(9).repeat(4, 1)  # simulate task design matrix X
     part_vec = np.kron(np.arange(4),np.ones((9,)))
-    simulate_full_VMF2(X,num_sub=11,part_vec = part_vec)
+    simulate_multi([X,X,X],num_sub=[6,10,8],
+                           part_vec = [part_vec,part_vec,part_vec])
