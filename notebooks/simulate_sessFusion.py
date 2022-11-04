1da88f4fc61a9cd9fcfbee74c0039f0c0594bc6b
dzhi1993 <r930710@gmail.com>
2022-11-04 10:40:38 -0400

    across sessions

--- notebooks/simulate_sessFusion.py
+++ notebooks/simulate_sessFusion.py
@@ -241,7 +241,117 @@ def _plot_maps(U, cmap='tab20', grid=None, offset=1, dim=(30, 30),
         plt.savefig(f'{row_labels}.eps', format='eps')
     plt.show()
 
-def do_simulation_sessFusion_dz(K=5, M=np.array([5,5],dtype=int), nsubj_list=None,
+def do_simulation_sessFusion_sess(K=5, M=np.array([5],dtype=int), nsubj_list=None, num_part=3,
+                                  width=10, low_kappa=3, high_kappa=30, plot_trueU=False):
+    """Run the simulation of common kappa vs separate kappas across
+       different sessions in the same set of subjects
+    Args:
+        K: the clusters number
+        P: the voxel number
+        nsubj_list:
+    Returns:
+        theta_all: All parameters at each EM iteration
+        Uerr_all: The absolute error between U and U_hat for each missing rate
+        U: The ground truth Us
+        U_nan_all: the ground truth Us with missing data
+        U_hat_all: the predicted U_hat for each missing rate
+    """
+    #Generate grid for easier visualization
+    grid = sp.SpatialGrid(width=width, height=width)
+    pm = ar.PottsModel(grid.W, K=K, remove_redundancy=False)
+    pm.random_smooth_pi(grid.Dist, theta_mu=20)
+
+    T = make_full_model(K=K, P=grid.P, nsubj_list=nsubj_list, num_part=num_part, M=M)
+    T.arrange.logpi = pm.logpi
+
+    if plot_trueU:
+        grid.plot_maps(pt.argmax(pm.logpi, dim=0), cmap='tab20', vmax=19, grid=[1, 1])
+        plt.savefig('trueU.eps', format='eps')
+        plt.show()
+        plt.clf()
+        grid.plot_maps(pt.exp(pm.logpi), cmap='jet', vmax=1, grid=(1, K), offset=1)
+        plt.savefig('true_prior.eps', format='eps')
+        plt.show()
+
+    # Initialize all kappas to be the high value
+    for em in T.emissions:
+        em.kappa = pt.full(em.kappa.shape, high_kappa)
+
+    # Making ambiguous boundaries by set the same V_k for k-neighbouring parcels
+    label_map = pt.argmax(T.arrange.logpi, dim=0).reshape(grid.dim)
+    _, base, idx = _compute_adjacency(label_map, 3)
+
+    # the parcels have same V in dataset1
+    idx_1 = pt.cat((idx, base.view(1)))
+    # the parcels have same V in dataset2
+    idx_2 = pt.tensor([i for i in label_map.unique() if i not in idx_1])
+    print(base, idx_1, idx_2)
+    for i, par in enumerate([idx_1, idx_2]):
+        # Making the V align to the first parcel
+        for j in range(1, len(par)):
+            T.emissions[i].V[:, par[j]] = T.emissions[i].V[:, par[0]]
+
+        # Set kappas of k-neighbouring parcels to low_kappa
+        for k in range(len(par)):
+            if not T.emissions[i].uniform_kappa:
+                T.emissions[i].kappa[par[k]] = low_kappa
+            else:
+                raise ValueError("The kappas of emission models need to be separate in simulation")
+
+    # Sampling individual Us and data
+    U,Y = T.sample()
+
+    Uerrors, U_indv, figs, Props = [], [], [], []
+    for common_kappa in [True, False]:
+        models = []
+        em_indx = [[0, 1], [0], [1], [0, 1]]
+        # Initialize three full models: dataset1, dataset2, dataset1 and 2
+        for j in range(3):
+            models.append(make_full_model(K=K,P=grid.P,M=M[em_indx[j+1]],
+                                          nsubj_list=nsubj_list[em_indx[j+1]],
+                                          common_kappa=common_kappa))
+            data = [Y[i] for i in em_indx[j+1]]
+            models[j].initialize(data)
+
+        # Fitting the full models
+        U_fit = []
+        for i,m in enumerate(models):
+            models[i],ll,theta,Uhat,first_ll = \
+                models[i].fit_em_ninits(n_inits=40, first_iter=7, iter=100, tol=0.01,
+                fit_emission=True, fit_arrangement=True,
+                init_emission=True, init_arrangement=True,
+                align = 'arrange')
+            U_fit.append(Uhat)
+
+        # Align full models to the true
+        MM = [T]+models
+        Prop = ev.align_models(MM)
+        figs.append(U_fit)
+        Props.append(Prop)
+
+        # Calculate and plot U reconstruction error
+        U1 = U[T.subj_ind[0],:]
+        U2 = U[T.subj_ind[1],:]
+
+        # Option 1: Using matching_U
+        U_recon_1, uerr_1 = ev.matching_U(U1, U_fit[0])
+        U_recon_2, uerr_2 = ev.matching_U(U2, U_fit[1])
+        U_recon, uerr = ev.matching_U(U, U_fit[2])
+        # TODO: Option 2: Using matching greedy
+
+        U_indv.append([U_recon_1, U_recon_2, U_recon])
+        Uerrors.append([uerr_1, uerr_2, uerr])
+
+        # Printing kappa fitting
+        Kappa = np.zeros((2,4,K))
+        for j,ei in enumerate(em_indx):
+            for k,i in enumerate(ei):
+                Kappa[i,j,:]=MM[j].emissions[k].kappa
+        print(Kappa.round(1))
+
+    return grid, U, U_indv, Uerrors, Props
+
+def do_simulation_sessFusion_subj(K=5, M=np.array([5,5],dtype=int), nsubj_list=None,
                                 width=10, low_kappa=3, high_kappa=30, plot_trueU=False):
     """Run the missing data simulation at given missing rate
     Args:
@@ -361,9 +471,48 @@ def simulation_1(width=30,
     Returns:
         Simulation result plots
     """
-    grid, U, U_indv, Uerrors, Props = do_simulation_sessFusion_dz(K=5, M=M, nsubj_list=nsub_list,
-                                                                  width=width, low_kappa=3,
-                                                                  high_kappa=30, plot_trueU=True)
+    grid, U, U_indv, Uerrors, Props = do_simulation_sessFusion_subj(K=5, M=M,
+                                                                    nsubj_list=nsub_list,
+                                                                    width=width,
+                                                                    low_kappa=3,
+                                                                    high_kappa=30,
+                                                                    plot_trueU=True)
+    plot_uerr(Uerrors, save=True)
+    plot_result(grid, Props, save=True)
+
+    # Plot all true individual maps
+    _plot_maps(U, cmap='tab20', dim=(width, width), row_labels='True', save=True)
+
+    # Plot fitted and aligned individual maps in dataset1, 2 and fusion
+    labels = ["commonKappa_", "separateKappa_"]
+    names = ["Dataset 1", "Dataset 2", "Dataset 1+2"]
+    for i, us in enumerate(U_indv):
+        for j in range(len(us)):
+            _plot_maps(us[j], cmap='tab20', dim=(width, width),
+                       row_labels=labels[i]+names[j], save=True)
+
+    pass
+
+def simulation_2(width=30,
+                 nsub_list=np.array([10]),
+                 M=np.array([10],dtype=int),
+                 num_part=3):
+    """Simulation of common kappa vs. separate kappas across subjects
+    Args:
+        width: The width and height of MRF grid
+        nsub_list: the list of number of subject per dataset (emission model)
+        M: The number of conditions per dataset (emission model)
+        num_part: the number of partitions (sessions)
+    Returns:
+        Simulation result plots
+    """
+    grid, U, U_indv, Uerrors, Props = do_simulation_sessFusion_sess(K=5, M=M,
+                                                                    nsubj_list=nsub_list,
+                                                                    num_part=num_part,
+                                                                    width=width,
+                                                                    low_kappa=3,
+                                                                    high_kappa=30,
+                                                                    plot_trueU=True)
     plot_uerr(Uerrors, save=True)
     plot_result(grid, Props, save=True)
 
@@ -385,5 +534,9 @@ if __name__ == '__main__':
     # do_simulation_sessFusion(5, nsub_list)
     # pass
 
-    simulation_1()
+    # 1. simulate across subjects
+    # simulation_1()
+
+    # 2. simulate across sessions in same set of subjects
+    simulation_2()
     pass
