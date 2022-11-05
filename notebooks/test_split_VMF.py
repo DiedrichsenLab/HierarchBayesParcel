974d02beb749d38142a4c13c686a867113320450
Jörn Diedrichsen <joern.diedrichsen@googlemail.com>
2022-11-04 20:39:38 -0400

    splitVMF

--- notebooks/test_split_VMF.py
+++ notebooks/test_split_VMF.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import Functional_Fusion.matrix as matrix
 # for testing and evaluating models
 import generativeMRF.evaluation as ev
-import generativeMRF.arrangements as ar 
+import generativeMRF.arrangements as ar
 import generativeMRF.emissions as em
 
 def make_joined(K=5,P=100,
@@ -23,28 +23,29 @@ def make_joined(K=5,P=100,
     U=np.kron(np.ones(int(P/K),),np.arange(K))
     U=np.kron(np.ones((n_subj,1)),U.reshape(1,-1))
     sess_vec = np.kron(np.arange(n_sess),np.ones(n_part*n_cond,))
-    # Different conditions across sessions 
+    # Different conditions across sessions
     c = np.kron(np.arange(n_cond),np.ones(n_part,))
     cond_vec = np.kron(np.ones(n_sess,),c)
-    cond_vec = cond_vec + sess_vec*n_cond 
+    cond_vec = cond_vec + sess_vec*n_cond
     part_vec = np.kron(np.ones(n_sess * n_cond,),np.arange(n_part))
 
     # Build emission model
     X = matrix.indicator(cond_vec)
     emM = em.MixVMF(K=K,P=P,part_vec = part_vec, X=X,uniform_kappa=True)
-    
-    # Set V to something informative
-    Vi = [] # List of individual V's 
-    for i in range(n_sess):
-        Vi.append(pt.randn(n_cond, K))
-        Vi[-1] = Vi[-1] / pt.sqrt(pt.sum(Vi[-1] ** 2, dim=0))
-    
 
-    V = pt.concat([Vi[0]*W,Vi[1]*(1-W)],dim=0)
+    # Set V to something informative
+    Vi = [] # List of individual V's
+    for i in range(n_sess):
+        Vi.append(pt.randint(0,2, (n_cond,K))*2-1)
+
+    if W is None:
+        V = pt.concat(Vi,dim=0)
+    else:
+        V = pt.concat([Vi[0]*W,Vi[1]*(1-W)],dim=0)
     V = V / pt.sqrt(pt.sum(V ** 2, dim=0))
 
     emM.V = V
-    emM.kappa = 5
+    emM.kappa = 10
     Y = emM.sample(pt.tensor(U))
     return Y,U,sess_vec,cond_vec,part_vec,emM
 
@@ -52,9 +53,10 @@ def simulate_split(K=5,P=100,
                 n_cond = 10,
                 n_sess = 2,
                 n_part = 2,
-                n_subj = 100):
+                n_subj = 100,
+                W=None):
     Y,U,sess_vec,cond_vec,part_vec,emT = make_joined(K,P,n_cond,
-                                            n_sess,n_part,n_subj)
+                                            n_sess,n_part,n_subj,W)
     emM =[]
     # Build overall emission model
     X = matrix.indicator(cond_vec)
@@ -63,10 +65,10 @@ def simulate_split(K=5,P=100,
 
     # Build emission model with Uniform / non-uniform kappa
     for uk in [True,False]:
-        for s in range(2):
+        for s in range(n_sess):
             X = matrix.indicator(cond_vec[sess_vec==s])
             emM.append(em.MixVMF(K=K,P=P,
-                part_vec = part_vec[sess_vec==s], 
+                part_vec = part_vec[sess_vec==s],
                 X=X,
                 uniform_kappa=uk))
             emM[-1].initialize(Y[:,sess_vec==s,:])
@@ -76,7 +78,7 @@ def simulate_split(K=5,P=100,
         LL.append(M.Estep())
     pass
 
-    # Plot result of simulation 
+    # Plot result of simulation
     plt.figure(figsize=(18,5))
 
 
@@ -89,28 +91,34 @@ def simulate_split(K=5,P=100,
     plt.title(f'Joint k={emM[0].kappa:0.2f}')
 
     plt.subplot(2,4,3)
-    V2 = np.r_[emM[1].V*emM[1].kappa,emM[2].V*emM[2].kappa]
+    V2=[]
+    for i in range(n_sess):
+        V2.append(emM[i+1].V*emM[i+1].kappa)
+    V2 = np.vstack(V2)
     V2  = V2 / np.sqrt(np.sum(V2**2,axis=0))
     plt.imshow(V2,vmin=-0.7,vmax=0.7)
     plt.title(f'k={emM[1].kappa:0.2f}\nk={emM[2].kappa:0.2f}')
 
     plt.subplot(2,4,4)
-    V2 = np.r_[emM[3].V*emM[3].kappa,emM[4].V*emM[4].kappa]
+    V2=[]
+    for i in range(n_sess):
+        V2.append(emM[i+1+n_sess].V*emM[i+1+n_sess].kappa)
+    V2 = np.vstack(V2)
     V2  = V2 / np.sqrt(np.sum(V2**2,axis=0))
     plt.imshow(V2,vmin=-0.7,vmax=0.7)
-    
+
     str=''
-    for i in [0,1]:
+    for i in range(n_sess):
         str += 'k='
         for k in range(K):
-            str += f' {emM[3+i].kappa[k]:0.1f}'
+            str += f' {emM[n_sess+1+i].kappa[k]:0.1f}'
         str += '\n'
     plt.title(str)
 
     plt.subplot(2,4,6)
     U1 = pt.softmax(LL[0][0,:,:5],dim=0)
     plt.imshow(U1,vmin=0,vmax=1)
-    
+
     plt.subplot(2,4,7)
     U2 = pt.softmax(LL[1][0,:,:5] + LL[2][0,:,:5],dim=0)
     plt.imshow(U2,vmin=0,vmax=1)
@@ -121,8 +129,8 @@ def simulate_split(K=5,P=100,
     pass
 
 if __name__ == '__main__':
-    simulate_split(n_cond=20)
-    
+    simulate_split(n_cond=500,n_sess=3)
+
 
 
     pass
