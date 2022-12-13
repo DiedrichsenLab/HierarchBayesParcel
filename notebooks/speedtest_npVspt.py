98a6e9f3c9b88f6b36e08460bcbe0de697bbd7bd
Joern Diedrichsen <joern.diedrichsen@googlemail.com>
2022-12-13 15:52:39 -0500

    Speed check - Cuda

--- notebooks/speedtest_npVspt.py
+++ notebooks/speedtest_npVspt.py
@@ -11,12 +11,12 @@ import time
 
 pt.set_default_tensor_type(pt.cuda.FloatTensor
                            if pt.cuda.is_available() else
-                           torch.FloatTensor)
+                           pt.FloatTensor)
 
 def mul_torch(x, y, n):
-    z = pt.mul(x, y).cuda()
+    z = pt.mul(x, y) # .cuda()
     for i in range(n-1):
-        z += pt.mul(x, y).cuda()
+        z += pt.mul(x, y)
     return z
 
 def mul_numpy(x, y, n):
@@ -26,30 +26,30 @@ def mul_numpy(x, y, n):
     return z
 
 if __name__ == '__main__':
-
-    tic = time.perf_counter()
+    N = 3
     x = pt.rand(200, 1000, 1000)  # Is FloatTensor
     y = pt.rand(200, 1000, 1000)
-    z = mul_torch(x, y, 100)
+    tic = time.perf_counter()
+    z = mul_torch(x, y, N)
     toc = time.perf_counter()
     print(f"Time of torch multiplication {toc - tic:0.4f} seconds")
 
     tic = time.perf_counter()
-    z = mul_torch(x, y, 100)
+    z = mul_torch(x, y, N)
     toc = time.perf_counter()
     print(f"Time of torch multiplication {toc - tic:0.4f} seconds")
 
     tic = time.perf_counter()
     x_ = x.data.cpu()
     y_ = y.data.cpu()
-    z_ = mul_numpy(x_, y_, 100)
+    z_ = mul_numpy(x_, y_, N)
     toc = time.perf_counter()
     print(f"Time of torch multiplication {toc - tic:0.4f} seconds")
 
     tic = time.perf_counter()
     x_ = x.data.cpu().numpy()
     y_ = y.data.cpu().numpy()
-    z_ = mul_numpy(x_, y_, 100)
+    z_ = mul_numpy(x_, y_, N)
     toc = time.perf_counter()
     print(f"Time of torch multiplication {toc - tic:0.4f} seconds")
 
