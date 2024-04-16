383fab1e0b19b63ccab8bc07ff14c49069df359c
Joern Diedrichsen <joern.diedrichsen@googlemail.com>
2024-04-16 11:58:29 -0400

    Documentation path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../HierarchBayesParcel'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
